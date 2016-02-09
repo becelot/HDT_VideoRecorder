4bfd8f51f0499d7a01699559ca4001fa19e86cb1
BBecker <benedict.becker@rwth-aachen.de>
2016-02-09 10:26:54 +0100

    Added ability to directly download OBS if not already installed

--- HDT_GameRecorder/HDT_GameRecorder/SettingsControl.xaml.cs
+++ HDT_GameRecorder/HDT_GameRecorder/SettingsControl.xaml.cs
@@ -14,6 +14,7 @@ using System.Windows.Navigation;
 using System.Windows.Shapes;
 using Hearthstone_Deck_Tracker.Enums;
 using HDT_GameRecorder.Utils;
+using System.Diagnostics;
 
 namespace HDT_GameRecorder
 {
@@ -29,6 +30,49 @@ namespace HDT_GameRecorder
             InitializeComponent();
             SetRecordedGameModes();
             SetOBSProfileNames();
+            SetOBSStatusNotification();
+        }
+
+        private void SetOBSStatusNotification()
+        {
+
+
+            if (OBSUtils.isObsInstalled())
+            {
+                //Layout
+                Label testLabel = new Label();
+                testLabel.Margin = new Thickness(5, 5, 5, 0);
+                testLabel.HorizontalAlignment = HorizontalAlignment.Center;
+                testLabel.Content = "OBS installation was found";
+                obsStackPanel.Children.Add(testLabel);
+            }
+            else {
+                //DockPanel
+                DockPanel dp = new DockPanel();
+                dp.Margin = new Thickness(5, 5, 5, 0);
+                dp.HorizontalAlignment = HorizontalAlignment.Stretch;
+
+                //Label
+                Label statusLabel = new Label();
+                statusLabel.Content = "OBS is not installed";
+                dp.Children.Add(statusLabel);
+
+                //Button
+                Button downloadButton = new Button();
+                downloadButton.Margin = new Thickness(5, 0, 0, 0);
+                downloadButton.Content = "Download OBS";
+                downloadButton.Click += DownloadButton_Click;
+                dp.Children.Add(downloadButton);
+
+                obsStackPanel.Children.Add(dp);
+            }
+
+            
+        }
+
+        private void DownloadButton_Click(object sender, RoutedEventArgs e)
+        {
+            Process.Start("http://google.de");
         }
 
         private void SetOBSProfileNames()
